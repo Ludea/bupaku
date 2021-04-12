63430022530438e4412fe51fdb3831b4656ed194
Ludea <ludovicw35@hotmail.com>
2021-04-12 17:12:10 +0200

    display stdout shell into textarea component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Box from '@material-ui/core/Box';
 import TextField from '@material-ui/core/TextField';
 import InputLabel from '@material-ui/core/InputLabel';
@@ -21,49 +21,58 @@ let args: any;
 let extensions: any;
 let child;
 
-function spawn(script: any) {
-  invoke("detect_os")
-  .then(data => {
-    switch (data) {
-      case 'windows': 
-        cmd = "powershell";
-        args = ['/C'];
-        extensions = '.bat'
-        break;
-      case 'macos': 
-        cmd = "sh"
-        args = ['-c'];
-        extensions = '.command'
-        break;
-      case 'linux': 
-        cmd = "sh"
-        args = ['-c'];
-        extensions = '.sh'
-        break;
-    }
-  })
-  .catch(data => console.log(data));
-  child = null
-  const command = new Command(cmd, [...args, script + extensions])
-  command.on('close', data => {
-    console.log("command finished with code : " + data.code + "and signal : " + data.signal)
-    child = null
-  })
-  command.on('error', error => console.log(error) )
-  command.stdout.on('data', line => console.log(line))
-  command.stderr.on('data', line => console.log(line))
-  
-  command.spawn()
-    .then(c => {
-      child = c
-    })
-   // .catch(onMessage)
-}
-
-
 function App() {
   const [UE4Path, setUE4Path] = useState<any>();
   const [UE4Version, setUE4Version] = useState<any>();
+  const stdoutput = useRef<any>();
+
+  function spawn(script: any) {
+    invoke("detect_os")
+    .then(data => {
+      switch (data) {
+        case 'windows': 
+          cmd = "powershell";
+          args = ['/C'];
+          extensions = '.bat'
+          break;
+        case 'macos': 
+          cmd = "sh"
+          args = ['-c'];
+          extensions = '.command'
+          break;
+        case 'linux': 
+          cmd = "sh"
+          args = ['-c'];
+          extensions = '.sh'
+          break;
+      }
+      child = null
+      const command = new Command(cmd, [...args, script])
+      command.on('close', data => {
+        child = null
+      })
+      command.on('error', error => {
+        stdoutput.current.value = error
+        console.log(JSON.stringify(error))
+      })
+      command.stdout.on('data', line => {
+        stdoutput.current.value = line
+        console.log(line)
+      })
+      
+      command.stderr.on('data', line => stdoutput.current.value = JSON.stringify(line))
+    
+      command.spawn()
+        .then(data => {
+          child = data
+          stdoutput.current.value = JSON.stringify(data)
+        })
+        .catch(error => {
+          console.log("error : " + JSON.stringify(error))
+        })
+      })
+      .catch(data => stdoutput.current.value = data );
+  }
 
   function openDialog() {
     open({
@@ -120,6 +129,7 @@ function App() {
         rows="10"
         defaultValue="Welcome"
         fullWidth
+        inputRef={stdoutput}
         margin="normal"
         variant="outlined"
         inputProps={{
