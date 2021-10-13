6a1996ac373be4a4907a516aeb5151c1db89736e
Ludea <ludovicw35@hotmail.com>
2021-10-13 22:17:58 +0200

    fix relative path with vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '': path.resolve(__dirname, 'src'),
-    },
-  },
-  plugins: [react()]
+  plugins: [react(), tsconfigPaths()]
 })
