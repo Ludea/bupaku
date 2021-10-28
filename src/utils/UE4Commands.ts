41474d94caaa5c13ecd257f4aaec3d847d2a9103
Ludea <ludovicw35@hotmail.com>
2021-10-28 20:32:34 +0200

    start BuildGraph even no Platform selected

--- src/utils/UE4Commands.ts
+++ src/utils/UE4Commands.ts
@@ -34,37 +34,39 @@ const runCommand = (arg: any, callback: any) => {
 };
 
 export const BuildGraph = (Platform: any, Path: any, callback: any ) => {
-   let UATarguments: string;
-   switch (Platform.name){
-     case "HostOnly": UATarguments = " -Set:HostPlatformOnly=" + Platform.value;
-     break;
-     case "Win64": UATarguments = " -Set:WithWin64=" + Platform.value;
-     break;
-     case "Win32": UATarguments = " -Set:WithWin32=" + Platform.value;
-     break;
-     case "Mac": UATarguments = " -Set:WithMac=" + Platform.value;
-     break;
-     case "Linux": UATarguments = " -Set:WithLinux=" + Platform.value;
-     break;
-     case "Android": UATarguments = " -Set:WithAndroid=" + Platform.value;
-     break;
-     case "IOS": UATarguments = " -Set:WithIOS=" + Platform.value;
-     break;
-     case "TVOS": UATarguments = " -Set:WithTVOS=" + Platform.value;
-     break;
-     case "Switch": UATarguments = " -Set:WithSwitch=" + Platform.value;
-     break;
-     case "PS4": UATarguments = " -Set:WithPS4=" + Platform.value;
-     break;
-     case "Xbox": UATarguments = " -Set:WithXboxOne=" + Platform.value;
-     break;
-     case "Lumin": UATarguments = " -Set:WithLumin=" + Platform.value;
-     break;
-     case "Hololens": UATarguments = " -Set:WithHololens=" + Platform.value;
-     break;
-     default: UATarguments = "";
-   }
-
+   let UATarguments: string = "";
+   if (Platform != undefined)
+   {
+      switch (Platform.name){
+        case "HostOnly": UATarguments =+ " -Set:HostPlatformOnly=" + Platform.value;
+        break;
+        case "Win64": UATarguments =+ " -Set:WithWin64=" + Platform.value;
+        break;
+        case "Win32": UATarguments =+ " -Set:WithWin32=" + Platform.value;
+        break;
+        case "Mac": UATarguments =+ " -Set:WithMac=" + Platform.value;
+        break;
+        case "Linux": UATarguments =+ " -Set:WithLinux=" + Platform.value;
+        break;
+        case "Android": UATarguments =+ " -Set:WithAndroid=" + Platform.value;
+        break;
+        case "IOS": UATarguments =+ " -Set:WithIOS=" + Platform.value;
+        break;
+        case "TVOS": UATarguments =+ " -Set:WithTVOS=" + Platform.value;
+        break;
+        case "Switch": UATarguments =+ " -Set:WithSwitch=" + Platform.value;
+        break;
+        case "PS4": UATarguments =+ " -Set:WithPS4=" + Platform.value;
+        break;
+        case "Xbox": UATarguments =+ " -Set:WithXboxOne=" + Platform.value;
+        break;
+        case "Lumin": UATarguments =+ " -Set:WithLumin=" + Platform.value;
+        break;
+        case "Hololens": UATarguments =+ " -Set:WithHololens=" + Platform.value;
+        break;
+        default: UATarguments = "prout";
+      }
+    }
    Extensions
     .then((extension: any) => {
      Target
