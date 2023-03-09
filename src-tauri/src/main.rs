37efa5dfd41328f153c7d895161998415e58dbe4
Ludea <ludovicw35@hotmail.com>
2023-03-09 12:11:47 +0000

    downgrade tauri and use new stronghold api

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,7 +3,7 @@
   windows_subsystem = "windows"
 )]
 
-use tauri_plugin_stronghold::TauriStronghold;
+use rand::distributions::{Alphanumeric, DistString};
 
 mod cmd;
 
@@ -16,7 +16,25 @@ struct Payload {
 
 fn main() {
   tauri::Builder::default()
-    .plugin(TauriStronghold::default())
+  .plugin(
+    tauri_plugin_stronghold::Builder::new(|password| {
+      let config = argon2::Config {
+        lanes: 2,
+        mem_cost: 50_000,
+        time_cost: 30,
+        thread_mode: argon2::ThreadMode::from_threads(2),
+        variant: argon2::Variant::Argon2id,
+        ..Default::default()
+      };
+
+      let salt = Alphanumeric.sample_string(&mut rand::thread_rng(), 12);
+      let key = argon2::hash_raw(password.as_ref(), salt.as_bytes(), &config)
+        .expect("failed to hash password");
+
+      key.to_vec()
+    })
+    .build(),
+  )
     .invoke_handler(tauri::generate_handler![
       cmd::pull,
       cmd::detect_os,
